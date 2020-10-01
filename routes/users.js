8fc09ef54366e64379c9daf36d934412fc024dca
muhammedfahis <fahisccc3@gmail.com>
2020-10-01 15:12:33 +0530

    basic paypal integrated

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,18 @@ const progress = require('progress-stream');
 const axios = require('axios');
 const FormData = require('form-data');
 const Address=require('../models/address');
+const paypal =require('paypal-rest-sdk');
+
+
+
+
+// paypal config
+
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'ATRdJYxiszV1z9a8znZHASfrY4RqhRgULjNtytARzB0gEKLjz92Tq88bUOTD1-aOHenPAfb6kzBVqVNd',
+  'client_secret': 'EPGTGXhvezBRAHql2lBxL1XuHKsPtibPO-wCjiM2Kq_OWY6AspHLVqNTz3r9zzQgnKia5u-1dyK5NQBl'
+});
 
 
 let otpId;
@@ -815,9 +827,12 @@ router.post('/edt_upload', (req, res) => {
   res.redirect('/users/myproject')
 });
 
+
+//  paypal complete works
 //  reading users address when preorder the item
 
-router.post('/address_save',(req,res)=>{
+
+router.post('/redirect_payment',(req,res)=>{
 
   const {email,name,phone,country,address}= req.body
   
@@ -829,7 +844,80 @@ router.post('/address_save',(req,res)=>{
     address:address
   });
   newAddress.save();
-})
+
+  const create_payment_json = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/users/success",
+        "cancel_url": "http://localhost:3000/users/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "Red Sox Hat",
+                "sku": "001",
+                "price": "25.00",
+                "currency": "USD",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "USD",
+            "total": "25.00"
+        },
+        "description": "Hat for the best team ever"
+    }]
+};
+
+paypal.payment.create(create_payment_json, function (error, payment) {
+  if (error) {
+      throw error;
+  } else {
+      for(let i = 0;i < payment.links.length;i++){
+        if(payment.links[i].rel === 'approval_url'){
+          res.redirect(payment.links[i].href);
+        }
+      }
+  }
+});
+});
+
+
+// success payment
+
+router.get('/success', (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    "payer_id": payerId,
+    "transactions": [{
+        "amount": {
+            "currency": "USD",
+            "total": "25.00"
+        }
+    }]
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+    if (error) {
+        console.log(error.response);
+        throw error;
+    } else {
+        console.log(JSON.stringify(payment));
+        res.send('Success');
+    }
+});
+});
+
+router.get('/cancel', (req, res) => res.send('Cancelled'));
+
+
+
+
 
 
 
